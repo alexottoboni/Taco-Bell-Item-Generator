bc3e1ec2b7c0dec8abccaeb0e8824216ef2412af
cooltoast <mpatel678@gmail.com>
2020-11-09 18:06:06 -0500

    Add precommit hook for Prettier

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
-Taco Bell Item Generator
-========
+# Taco Bell Item Generator
+
 ### Taco Bell Item Generator
 
 Using the latest in Taco Bell technology to create the items of your wildest dreams.
 
-Technology
-----------
+## Technology
 
 Through a state-of-the-art totally secret algorithm, we can generate tens, even hundreds, of unique Taco Bell items.
 
-Browser Compatibility
----------------------
+## Browser Compatibility
 
 Please take any questions or concerns about browser compatibility to: https://www.google.com/chrome/browser/desktop/index.html
 
-FAQ
--------
+## FAQ
 
 **1) Will Taco Bell actually make me an item from this site?**
 
--- assets/generator.js
+++ assets/generator.js
@@ -1,38 +1,62 @@
-const prefixes = ['Breakfast', 'BLT', 'Fiery',
-                  'Locos', 'Cool Ranch', 'Nacho Cheese',
-                  'Crunchy', 'Beefy', 'Fritos', 'Smothered',
-                  'Cheesy', 'Fiesta', 'XXL',
-                  'Triple Layer', 'Spicy', 'Cheddar',
-                  'Grilled', 'Baja', 'Naked'],
-      meats = ['Chicken', 'Beef', 'Bean', 'Doritos', 'Tofu'],
-      items = ['Taco', 'Burrito', 'Gordita',
-               'Salad', 'Nacho', 'Chalupa',
-               'Crunchwrap', 'Mexican Pizza', 'Empanada',
-               'Enchirito', 'Quesadilla'],
-      suffixes = ['Supreme', 'Cantina', 'Slider', 'Box', '12 pack', 'Crunch'];
+const prefixes = [
+    "Breakfast",
+    "BLT",
+    "Fiery",
+    "Locos",
+    "Cool Ranch",
+    "Nacho Cheese",
+    "Crunchy",
+    "Beefy",
+    "Fritos",
+    "Smothered",
+    "Cheesy",
+    "Fiesta",
+    "XXL",
+    "Triple Layer",
+    "Spicy",
+    "Cheddar",
+    "Grilled",
+    "Baja",
+    "Naked",
+  ],
+  meats = ["Chicken", "Beef", "Bean", "Doritos", "Tofu"],
+  items = [
+    "Taco",
+    "Burrito",
+    "Gordita",
+    "Salad",
+    "Nacho",
+    "Chalupa",
+    "Crunchwrap",
+    "Mexican Pizza",
+    "Empanada",
+    "Enchirito",
+    "Quesadilla",
+  ],
+  suffixes = ["Supreme", "Cantina", "Slider", "Box", "12 pack", "Crunch"];
 
-const getRandomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 function generateItem() {
-  return [prefixes, meats, items, suffixes].map(getRandomElement).join(' ');
+  return [prefixes, meats, items, suffixes].map(getRandomElement).join(" ");
 }
 
 function setItemOnscreen() {
-  const textElement = document.getElementById('tacobell-item');
-  textElement.style.animation = 'none';
-  setTimeout(function() {
-    textElement.style.animation = '';
+  const textElement = document.getElementById("tacobell-item");
+  textElement.style.animation = "none";
+  setTimeout(function () {
+    textElement.style.animation = "";
   }, 10);
 
   textElement.innerHTML = generateItem();
-  textElement.style.fontSize = '60pt';
-  textElement.style.color = 'rgb(40, 40, 40)';
+  textElement.style.fontSize = "60pt";
+  textElement.style.color = "rgb(40, 40, 40)";
 
-  if (textElement.innerHTML.indexOf('XXL') !== -1) {
-    textElement.style.fontSize = '100pt';
+  if (textElement.innerHTML.indexOf("XXL") !== -1) {
+    textElement.style.fontSize = "100pt";
   }
 
-  if (textElement.innerHTML.indexOf('Fiery') !== -1) {
-    textElement.style.color = 'rgb(240, 0, 0)';
+  if (textElement.innerHTML.indexOf("Fiery") !== -1) {
+    textElement.style.color = "rgb(240, 0, 0)";
   }
 }
